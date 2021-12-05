15595934d9e9a2686a46907ae6e76eff835ebcc0
eryner-z <stfn.bgr@gmail.com>
2021-12-05 19:30:05 +0100

    Initial gameboard module

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const gameBoard = (() => {
+    const row1 = [null, null, null];
+    const row2 = [null, null, null];
+    const row3 = [null, null, null];
+    return {
+        row1,
+        row2,
+        row3,
+    };
+})();
