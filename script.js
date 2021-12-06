c7928ba40b559593adc4126c70f4115ae68edf21
eryner-z <stfn.bgr@gmail.com>
2021-12-06 21:21:22 +0100

    Render gameBoardArray in DOM

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 
 // creates a gameboard object
 const gameBoardModule = (() => {
-            const gameBoardArray = ["X", "X", "X", "0", "0", "0", "X", "X", "X"];
+            const gameBoardArray = ["X", "O", "X", "0", "0", "0", "X", "X", "X"];
             const gameBoard = document.getElementById("gameboard");
             const cells = document.querySelectorAll("[data-cell]");
             cells.forEach(cell => {
                 cell.addEventListener("click", something, {once: true});
             });
-
-            function something(event) {
-                console.log("clicked");
+            function something(event) { 
+                console.log("test"); // add symbol of the current player
             }
             return {
                 gameBoardArray,
@@ -26,6 +25,13 @@ const createPlayer = (playerName, symbol) => {
         symbol,
     }
 }
+
+// render gameBoard-data
+const renderGameBoard = () => {
+    gameBoardModule.gameBoardArray.forEach(function(item, i) {
+        gameBoardModule.cells[i].textContent = item;
+    });
+    }
 
 // define winning conditions
 // define whose turn it is
